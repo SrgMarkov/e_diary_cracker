faef80facbec645b01d8a44b1f60fff5cfbc9708
SrgMarkov <107784915+SrgMarkov@users.noreply.github.com>
2023-05-28 12:43:28 +0400

    Изменено название переменной praise_phrases + вынесена в глобальную

--- scripts.py
+++ scripts.py
@@ -3,6 +3,18 @@ from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from datacenter.models import Lesson, Schoolkid, Mark, Chastisement, Commendation
 
 
+praise_phrases = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!',
+                  'Ты меня приятно удивил!', 'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!',
+                  'Именно этого я давно ждал от тебя!', 'Сказано здорово – просто и ясно!',
+                  'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!',
+                  'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!',
+                  'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!',
+                  'Ты на верном пути!', 'Здорово!', 'Это как раз то, что нужно!', 'Я тобой горжусь!',
+                  'С каждым разом у тебя получается всё лучше!', 'Мы с тобой не зря поработали!',
+                  'Я вижу, как ты стараешься!', 'Ты растешь над собой!', 'Ты многое сделал, я это вижу!',
+                  'Теперь у тебя точно все получится!']
+
+
 def fix_marks(schoolkid):
     try:
         child = Schoolkid.objects.get(full_name__contains=schoolkid)
@@ -28,23 +40,13 @@ def remove_chastisements(schoolkid):
 
 
 def create_commendation(schoolkid, lesson, year=6, group='А'):
-    praise = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!',
-              'Ты меня приятно удивил!', 'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!',
-              'Именно этого я давно ждал от тебя!', 'Сказано здорово – просто и ясно!',
-              'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!',
-              'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!',
-              'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!',
-              'Ты на верном пути!', 'Здорово!', 'Это как раз то, что нужно!', 'Я тобой горжусь!',
-              'С каждым разом у тебя получается всё лучше!', 'Мы с тобой не зря поработали!',
-              'Я вижу, как ты стараешься!', 'Ты растешь над собой!', 'Ты многое сделал, я это вижу!',
-              'Теперь у тебя точно все получится!']
     praised_lesson = Lesson.objects.filter(group_letter=group, year_of_study=int(year), subject__title=lesson)\
         .order_by('-date').first()
     if praised_lesson is None:
         return print('Название урока введено с ошибкой')
     try:
         child = Schoolkid.objects.get(full_name__contains=schoolkid)
-        Commendation.objects.create(text=random.choice(praise), created=praised_lesson.date, schoolkid=child,
+        Commendation.objects.create(text=random.choice(praise_phrases), created=praised_lesson.date, schoolkid=child,
                                     subject=praised_lesson.subject, teacher=praised_lesson.teacher)
     except MultipleObjectsReturned as error:
         print(f'Ошибка при выполнении: {error}')
